ce7541a2274613e7987e397e195ad742656fa7c7
Ruel Paya <ruel.paya@gmail.com>
2024-06-22 16:12:47 +0800

    install dependency
    package-lock.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
-import SearchBar from './Searchbar/Searchbar';
+import SearchBar from './SearchBar/SearchBar';
 import { getAPI } from 'pixabay-api';
 import Button from './Button/Button';
 import styles from './App.module.css';
-import { render } from "@testing-library/react";
 
 class App extends Component {
   state = {
@@ -26,76 +25,74 @@ class App extends Component {
     ) {
       await this.fetchImages();
     }
-
   }
+
   fetchImages = async () => {
-    this.setState({ isLoading: true, isError: false});
+    this.setState({ isLoading: true, isError: false });
     const { searchQuery, currentPage } = this.state;
     try {
       const response = await getAPI(searchQuery, currentPage);
       console.log(response);
       const { totalHits, hits } = response;
       this.setState(prevState => ({
-        images: currentPage === 1? hits : [...prevState.images, ...hits],
+        images: currentPage === 1 ? hits : [...prevState.images, ...hits],
         isLoading: false,
         isEnd: prevState.images.length + hits.length >= totalHits,
-
       }));
       if (hits.length === 0) {
         alert('No images found. Try a different search.');
         return;
       }
     } catch (error) {
-      this.setState({ isLoading: false, isError: true});
+      this.setState({ isLoading: false, isError: true });
       alert(`An error occurred while fetching data: ${error}`);
     }
   };
+
   handleSearchSubmit = query => {
     const normalizedQuery = query.trim().toLowerCase();
-    if ( normalizedQuery === '') {
-      alert (`Empty string is not a valid search query. Please type again.`);
+    const { searchQuery } = this.state;
+
+    if (normalizedQuery === '') {
+      alert('Empty string is not a valid search query. Please type again.');
       return;
     }
 
-    if (normalizedQuery === normalizedCurrentQuery) {
-      alert(
-          `Search query is the same as the previous one. Please provide a new search query.`
-      );
+    if (normalizedQuery === searchQuery.toLowerCase()) {
+      alert('Search query is the same as the previous one. Please provide a new search query.');
       return;
     }
-if (normalizedQuery !== normalizedCurrentQuery) {
-  this.state({
-    searchQuery: normalizedQuery,
-    currentPage: 1,
-    images: [],
-    isEnd: false,
-  });
-}
-    
+
+    this.setState({
+      searchQuery: normalizedQuery,
+      currentPage: 1,
+      images: [],
+      isEnd: false,
+    });
   };
+
   handleLoadMore = () => {
     if (!this.state.isEnd) {
       this.setState(prevState => ({ currentPage: prevState.currentPage + 1 }));
-} else {
-  alert ( "You've reached the end of the search results.");
+    } else {
+      alert("You've reached the end of the search results.");
+    }
+  };
 
+  render() {
+    const { images, isLoading, isError, isEnd } = this.state;
+    return (
+      <div className={styles.App}>
+        <SearchBar onSubmit={this.handleSearchSubmit} />
+        <ImageGallery images={images} />
+        {isLoading && <Loader />}
+        {!isLoading && !isError && images.length > 0 && !isEnd && (
+          <Button onClick={this.handleLoadMore} />
+        )}
+        {isError && <p>Something went wrong. Please try again later.</p>}
+      </div>
+    );
+  }
 }
-};
 
-render() {
-  const { images, isLoading, isError, isEnd } = this.state;
-return (
-  <div className={styles.App}>
-    <SearchBar onSubmit ={this.handleSearchSubmit} />
-    <ImageGallery images={images} />
-    {isLoading && <Loader />}
-    {isLoading && !isError && images.length > 0 && !isEnd && (
-      <Button onClick={this.handleLoadMore} />
-    )}
-    {isError && <p>Something went wrong .Please try again later .</p>}
-
-  </div>
-);
-}
-}
 export default App;
