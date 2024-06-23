573498eb47bbb4788bbbdfcda37a23022cd89c02
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-06-23 16:47:04 +0800

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styles from './Searchbar.module.css';
+import styles from './SearchBar.module.css';
 
 class SearchBar extends Component {
   static propTypes = {
