a2d4cf0d507a197b54b1b22e99e298879482ad43
Ruel Paya <ruel.paya@gmail.com>
2024-06-23 21:18:23 +0800

    load spinner
    edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import SearchBar from './SearchBar/SearchBar';
 import ImageGallery from './ImageGallery/ImageGallery';
-import Button from './Button/Button';
 import Loader from './Loader/Loader';
+import SearchBar from './SearchBar/SearchBar';
 import { getAPI } from 'pixabay-api';
+import Button from './Button/Button';
 import styles from './App.module.css';
 
 class App extends Component {
@@ -14,6 +14,16 @@ class App extends Component {
     isLoading: false,
     isError: false,
     isEnd: false,
+    showModal: false,
+    selectedImage: null,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  handleImageClick = image => {
+    this.setState({ selectedImage: image, showModal: true });
   };
 
   async componentDidUpdate(_prevProps, prevState) {
@@ -29,20 +39,16 @@ class App extends Component {
 
   fetchImages = async () => {
     this.setState({ isLoading: true, isError: false });
-
     const { searchQuery, currentPage } = this.state;
-
     try {
       const response = await getAPI(searchQuery, currentPage);
-      console.log(response);
+      console.log('Fetched images:', response.hits);
       const { totalHits, hits } = response;
-
       this.setState(prevState => ({
         images: currentPage === 1 ? hits : [...prevState.images, ...hits],
         isLoading: false,
         isEnd: prevState.images.length + hits.length >= totalHits,
       }));
-
       if (hits.length === 0) {
         alert('No images found. Try a different search.');
         return;
@@ -55,29 +61,23 @@ class App extends Component {
 
   handleSearchSubmit = query => {
     const normalizedQuery = query.trim().toLowerCase();
-    const normalizedCurrentQuery = this.state.searchQuery.toLowerCase();
-
     if (normalizedQuery === '') {
-      alert(`Empty string is not a valid search query. Please type again.`);
+      alert('Empty string is not a valid search query. Please type again.');
       return;
     }
 
-    if (normalizedQuery === normalizedCurrentQuery) {
-      alert(
-        `Search query is the same as the previous one. Please provide a new search query.`
-      );
+    if (normalizedQuery === this.state.searchQuery) {
+      alert('Search query is the same as the previous one. Please provide a new search query.');
       return;
     }
 
-    // Only update the state and fetch images if the new query is different
-    if (normalizedQuery !== normalizedCurrentQuery) {
-      this.setState({
-        searchQuery: normalizedQuery,
-        currentPage: 1,
-        images: [],
-        isEnd: false,
-      });
-    }
+    console.log('Search query submitted:', normalizedQuery);
+    this.setState({
+      searchQuery: normalizedQuery,
+      currentPage: 1,
+      images: [],
+      isEnd: false,
+    });
   };
 
   handleLoadMore = () => {
@@ -89,11 +89,11 @@ class App extends Component {
   };
 
   render() {
-    const { images, isLoading, isError, isEnd } = this.state;
+    const { images, isLoading, isError, isEnd, showModal, selectedImage } = this.state;
     return (
       <div className={styles.App}>
         <SearchBar onSubmit={this.handleSearchSubmit} />
-        <ImageGallery images={images} />
+        <ImageGallery images={images} onImageClick={this.handleImageClick} />
         {isLoading && <Loader />}
         {!isLoading && !isError && images.length > 0 && !isEnd && (
           <Button onClick={this.handleLoadMore} />
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TailSpin } from 'react-loader-spinner';
 import styles from './Loader.module.css';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className={styles.Loader}>
-        <div className={styles.loaderBox}>
-          <TailSpin color="#00BFFF" height={80} width={80} />
-        </div>
-      </div>
-    );
-  }
-}
+const Loader = () => (
+  <div className={styles.Loader}>
+    <div className={styles.loaderBox}>
+      <TailSpin color="#00BFFF" height={80} width={80} />
+    </div>
+  </div>
+);
 
 export default Loader;
